248484552c5e39222c7c077b6d71a20b6fb8c472
merrypapa <65912136+merrypapa@users.noreply.github.com>
2020-08-07 23:03:57 -0700

    submit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 const country = "Country";
 const selectElement = document.querySelector("select");
+const countryIndex = { Korea: 1, Greece: 2, Turkey: 3, Finland: 4 };
+
+function valueStorage(value) {
+  localStorage.setItem(country, value);
+}
+
 function getValue(event) {
   const value = event.target.value;
-  localStorage.setItem(country, value);
-  // const result = localStorage.getItem(country);
-  console.log("hi");
+  valueStorage(value);
 }
 
 function init() {
-  selectElement.addEventListener("change", getValue);
+  const savedCountry = localStorage.getItem(country);
+  if (savedCountry === null) {
+    selectElement.addEventListener("change", getValue);
+  } else {
+    const savedIndex = countryIndex[savedCountry];
+    selectElement.options.selectedIndex = savedIndex;
+    selectElement.addEventListener("change", getValue);
+  }
 }
 
 init();
